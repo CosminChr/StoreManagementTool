f568d8749385cd788fb6ccafa835edd24c5ef45c
home <cosmin.chiriaac@gmail.com>
2023-01-05 01:23:10 +0200

    refactored reositories

--- src/main/java/com/assignment/storemanagementtool/repository/BuyerRepository.java
+++ src/main/java/com/assignment/storemanagementtool/repository/BuyerRepository.java
@@ -3,5 +3,8 @@ package com.assignment.storemanagementtool.repository;
 import com.assignment.storemanagementtool.entity.Buyer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BuyerRepository extends JpaRepository<Buyer, Long> {
+  Optional<Buyer> findByFirstNameAndLastName(String firstName, String lastName);
 }
--- src/main/java/com/assignment/storemanagementtool/repository/OrderRepository.java
+++ src/main/java/com/assignment/storemanagementtool/repository/OrderRepository.java
@@ -4,10 +4,12 @@ package com.assignment.storemanagementtool.repository;
 import com.assignment.storemanagementtool.entity.Buyer;
 import com.assignment.storemanagementtool.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-  public List<Order> findByBuyer(Buyer buyer);
-  public List<Order> deleteByBuyer(Buyer buyer);
+  @Query(value = "SELECT o FROM Order o WHERE o.buyer.firstName = :firstName AND o.buyer.lastName = :lastName ")
+  List<Order> findByBuyerFirstNameAndLastName(String firstName, String lastName);
+  List<Order> deleteByBuyer(Buyer buyer);
 }
