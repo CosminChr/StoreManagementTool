e96d88c17a28bcbc9bcfa5ebcbfe59092ae5c78d
home <cosmin.chiriaac@gmail.com>
2023-01-05 01:25:35 +0200

    Moved the updateOrder endpoint into a new controller - the AdminOrderController

--- src/main/java/com/assignment/storemanagementtool/controller/AdminOrderController.java
+++ src/main/java/com/assignment/storemanagementtool/controller/AdminOrderController.java
@@ -4,31 +4,21 @@ import com.assignment.storemanagementtool.dto.OrderDTO;
 import com.assignment.storemanagementtool.service.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/orders")
 @AllArgsConstructor
-@Secured("ROLE_BUYER")
-public class BuyerOrderController {
+@Secured("ROLE_ADMIN")
+public class AdminOrderController {
 
   private OrderService orderService;
 
-  @PostMapping("/")
-  public OrderDTO createOrder(@RequestBody OrderDTO orderDTO, Authentication auth) {
-    return orderService.createOrder(orderDTO, auth);
-  }
-
-  @GetMapping("/")
-  public List<OrderDTO> findUserOrders(Authentication auth) {
-    return orderService.findUserOrders(auth);
+  @PutMapping("/")
+  public OrderDTO updateOrder(@RequestBody OrderDTO orderDTO) {
+    return orderService.updateOrder(orderDTO);
   }
 }
