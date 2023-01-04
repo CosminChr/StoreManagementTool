366f3bbd7862d6aa66762dda0e52b529085242c6
home <cosmin.chiriaac@gmail.com>
2023-01-05 01:19:30 +0200

    cleaned up the BuyerController class

--- src/main/java/com/assignment/storemanagementtool/controller/BuyerController.java
+++ src/main/java/com/assignment/storemanagementtool/controller/BuyerController.java
@@ -3,9 +3,7 @@ package com.assignment.storemanagementtool.controller;
 import com.assignment.storemanagementtool.dto.BuyerDTO;
 import com.assignment.storemanagementtool.service.BuyerService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +21,4 @@ public class BuyerController {
     return buyerService.register(buyerDTO);
   }
 
-  @Secured("ROLE_BUYER")
-  @GetMapping("/hello")
-  public String hello(@RequestBody BuyerDTO buyerDTO) {
-    return "test";
-  }
 }
